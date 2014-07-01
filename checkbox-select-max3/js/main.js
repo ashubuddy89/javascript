f3ce6cf979cae3b51d443660eedc804a53e7bf15
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-01 18:11:06 +0530

    created separate methods for click event and used addEventlistner instead of onclick

--- checkbox-select-max3/js/main.js
+++ checkbox-select-max3/js/main.js
@@ -1,66 +1,70 @@
 var LimitSelectDays = function () {
-  this.setAttributes();
-  this.init();
+
+  this.pageTitle = pageTitle;
+  this.form = form; 
+  this.maxDays = 3;
+  this.unselectAll = unselectAll;
+  this.checkBox = checkBox;
+  this.storeSelectedDays = [];
 }
 
 LimitSelectDays.prototype = {
-  setAttributes: function () {
-    this.pageTitle = document.title;
-    this.form = document.getElementById("form");
-    this.maxDays = 3;
-    this.none = document.getElementById("none");
-    this.checkBox = document.getElementsByClassName("day");
-    this.storeSelectedDays = [];
-  },
-
-  init: function () {
-    this.setPageHeading();
-    this.checkBoxBindEvent();
-  },
-
-  checkBoxBindEvent: function () {
-    this.checkMaxThreeInputCheckBox();
-    this.uncheckAllInputCheckBox();
-  },
 
   setPageHeading: function () {
     document.getElementById("page-title").innerHTML = "Exercise " + this.pageTitle;
   },
+  
+  checkBoxBindEvent: function () {
+    this.checkBoxBind();
+    this.uncheckAllInputCheckBox();
+  },
 
-  checkMaxThreeInputCheckBox: function () {
+  checkBoxBind: function () {
     var obj = this;
 
     for (i = 0; i < obj.checkBox.length; i++) {
-      this.checkBox[i].onclick = function () {
+
+      this.checkBox[i].addEventListener('click', function () {
         if (this.checked) {
-          obj.none.checked = false;
+          obj.unselectAll.checked = false;
           obj.storeSelectedDays.push(this.value);
         } else if (!this.checked) {
           obj.storeSelectedDays.splice(obj.storeSelectedDays.indexOf(this.value), 1);
         }
-
-        if (obj.storeSelectedDays.length > obj.maxDays) {
-          obj.storeSelectedDays.pop();
-          this.checked = false;
-          alert("you can't select more than 3 days. you have already selected " + obj.storeSelectedDays[0] + ", " + obj.storeSelectedDays[1] + " and " + obj.storeSelectedDays[2]);
-        }
-      }
+        obj.checkMaxThree(obj, this);        
+      })
     }
   },
 
+  checkMaxThree: function(obj, elem){
+    if (obj.storeSelectedDays.length > obj.maxDays) {
+      obj.storeSelectedDays.pop();
+      elem.checked = false;
+      alert("you can't select more than 3 days. you have already selected " + obj.storeSelectedDays[0] + ", " + obj.storeSelectedDays[1] + " and " + obj.storeSelectedDays[2]);
+    }
+  },
+  
   uncheckAllInputCheckBox: function () {
     var obj = this;
-    obj.none.onclick = function () {
+    obj.unselectAll.addEventListener('click', function () {
       for (i = 0; i < obj.checkBox.length; i++) {
         obj.checkBox[i].checked = false;
         obj.storeSelectedDays.splice(0, obj.checkBox.length);
-        console.log(obj.storeSelectedDays)
       }
-    }
+    })
   },
 
 }
 
 window.onload = function () {
+  //declared variables
+  pageTitle = document.title;
+  form = document.getElementById("form");
+  checkBox = document.getElementsByClassName("day");
+  unselectAll = document.getElementById("none");
+
   var limit_select_days = new LimitSelectDays();
+      //methods
+      limit_select_days.setPageHeading();
+      limit_select_days.checkBoxBindEvent();
 }
