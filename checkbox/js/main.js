193825e3cf17804bd83d8a59c0d7257968dbe172
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-01 17:29:55 +0530

    removed white spaces

--- checkbox/js/main.js
+++ checkbox/js/main.js
@@ -8,7 +8,6 @@ checkAllorNone.prototype = {
   SetPageHeading: function(){
     document.getElementById("page-title").innerHTML = "Exercise " + this.pageTitle;
   },
-  
 
   SetCheckboxStatus: function(value) { //method name
     for(var i=0; i < this.form.length; i++){
@@ -34,13 +33,10 @@ checkAllorNone.prototype = {
       obj.SetCheckboxStatus(false);
     });
   },
-
-  
 }
 
 window.onload = function(){
   var checkall_or_none = new checkAllorNone("form");
-
   checkall_or_none.SetPageHeading();
   checkall_or_none.bindEvents();
 }
