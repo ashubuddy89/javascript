21b3421c958f75e7357588e372b02b419096c1f9
Ashwani Bachchas <ashubuddy89@gmail.com>
2014-07-04 19:06:44 +0530

    Removed Notification validation

--- form-validation/js/main.js
+++ form-validation/js/main.js
@@ -1,4 +1,4 @@
-var validateForm = function () {
+var Form = function () {
   this.form = document.getElementById("form");
   this.validateInputFields = document.getElementsByClassName("validate");
   this.textarea = document.getElementById("aboutme");
@@ -7,56 +7,52 @@ var validateForm = function () {
   this.blankFileds = [];
 }
 
-validateForm.prototype = {
-
-  validateAllFields: function(){
-    obj = this;
-    obj.blankFileds = [];
-      for(i = 0; i < obj.validateInputFields.length; i++){
-        if(obj.validateInputFields[i].value.trim() == ''){
-            obj.blankFileds.push(obj.validateInputFields[i].getAttribute("name"));
-        }
+Form.prototype.validateAllFields = function(){
+  obj = this;
+  obj.blankFileds = [];
+    for(i = 0; i < obj.validateInputFields.length; i++){
+      if(obj.validateInputFields[i].value.trim() == ''){
+          obj.blankFileds.push(obj.validateInputFields[i].getAttribute("name"));
       }
-      if(obj.blankFileds.length > 0){
-        alert(obj.blankFileds.join(", ") + " can't be empty");
-        return false;
-      }
-      return true;
-  },
-
-  validateAboutmeLength: function(){
-    if(this.textarea.value.trim().length < this.aboutMaxLength) {
-      alert("Please enter atleast 50 characters to write about yourself.");
-      this.textarea.focus();
+    }
+    if(obj.blankFileds.length > 0){
+      alert(obj.blankFileds.join(", ") + " can't be empty");
       return false;
     }
     return true;
-  },
+}
 
-  validateNotification: function(){
-    if(!(this.notification.checked)){
-        alert("Please click on checkbox to recieve notifications");
-        this.notification.focus();
-        return false;
+Form.prototype.validateAboutmeLength = function(){
+  if(this.textarea.value.trim().length < this.aboutMaxLength) {
+    // alert("Please enter atleast 50 characters to write about yourself.");
+    this.textarea.focus();
+    return false;
+  }
+  return true;
+}
+
+Form.prototype.validateNotification = function(){
+  if(!(this.notification.checked)){
+      alert("Please click on checkbox to recieve notifications");
+      this.notification.focus();
+  }
+  return true;
+}
+
+Form.prototype.submitForm = function(){
+  var obj = this;
+  obj.form.onsubmit = function(){
+    if(obj.validateAllFields() & obj.validateAboutmeLength() & obj.validateNotification()) {
+      return true;
     }
-    return true;
-  },
-
-  submitForm: function(){
-    var obj = this;
-    obj.form.onsubmit = function(){
-      if(obj.validateAllFields() & obj.validateAboutmeLength() & obj.validateNotification()) {
-        return true;
-      }
-      else {
-        return false;
-      }
+    else {
+      return false;
+    }
       // return (obj.validateAllFields() && obj.validateAboutmeLength() && obj.validateNotification());
-    }
   }
 }
 
 window.onload = function () {
-  var validate_form = new validateForm();
-      validate_form.submitForm();
+  var validateForm = new Form();
+      validateForm.submitForm();
 }
